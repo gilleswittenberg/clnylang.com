65275a4da7636b588afe31ebd277e90fffd4bbd2
Gilles Wittenberg <git@gilleswittenberg.com>
2019-04-13 22:17:22 +0200

    Updates

---
+++ precache-manifest.47299faddd5258119862e3b3401d8eeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a720907ff9ad3003d3d3",
+    "url": "/static/css/main.432ab2fd.chunk.css"
+  },
+  {
+    "revision": "a720907ff9ad3003d3d3",
+    "url": "/static/js/main.df8f8ff2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "36a68524617227943a34",
+    "url": "/static/js/2.1906896e.chunk.js"
+  },
+  {
+    "revision": "845fa119f322407110626b74dbd4f69f",
+    "url": "/index.html"
+  }
+];
