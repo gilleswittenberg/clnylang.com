ef4a04596de8be716f292b1e9ec0be09cf3ac611
Gilles Wittenberg <git@gilleswittenberg.com>
2019-04-13 22:21:27 +0200

    Updates

---
+++ precache-manifest.d06d9fa59ac02c0d52b63635e6e4fc37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7db7463476d150282a8c",
+    "url": "/clnylang.com/static/css/main.432ab2fd.chunk.css"
+  },
+  {
+    "revision": "7db7463476d150282a8c",
+    "url": "/clnylang.com/static/js/main.1b50289e.chunk.js"
+  },
+  {
+    "revision": "de5d3e6db144b6b0b0ea",
+    "url": "/clnylang.com/static/js/runtime~main.ace66501.js"
+  },
+  {
+    "revision": "36a68524617227943a34",
+    "url": "/clnylang.com/static/js/2.1906896e.chunk.js"
+  },
+  {
+    "revision": "1b36e9aefdf96ff5f542c8d34909cb1d",
+    "url": "/clnylang.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47299faddd5258119862e3b3401d8eeb.js"
+  "/clnylang.com/precache-manifest.d06d9fa59ac02c0d52b63635e6e4fc37.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/clnylang.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
